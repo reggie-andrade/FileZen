39bf9fb5de0c15f6813e64e044d5e70ab5daa31e
sync144 <82534288+sync144@users.noreply.github.com>
2023-03-19 16:09:38 -0400

    Updated AddSortingOption()
    Added entryboxes for other options

--- src/main.py
+++ src/main.py
@@ -57,8 +57,8 @@ def addSortingOption():
     # New window setup
     wnSortOpt = Toplevel(wn)
     wnSortOpt.title("Add file sorting option")
-    wnSortOptWidth = 850
-    wnSortOptHeight = 350
+    wnSortOptWidth = 1100
+    wnSortOptHeight = 450
     wnSortOpt.geometry(f"{wnSortOptWidth}x{wnSortOptHeight}")
 
     selectVar = StringVar()
@@ -88,15 +88,17 @@ def addSortingOption():
     conditionResult.grid(column=2, row=2, padx=10, pady=40)
 
     ifEntryVar = StringVar()
-    ifConditionEntry = ttk.Entry(wnSortOpt, width=17)
-
+    ifConditionEntry = ttk.Entry(wnSortOpt, width=17, textvariable=ifEntryVar)
+    
     def selectSpecificDir():
         selectedFolder = filedialog.askdirectory()
+        dirSelectVar.set(selectedFolder)
+        wnSortOpt.lift()
 
-    dirSelect = ttk.Button(wnSortOpt, text="Select Folder", command=selectSpecificDir)
-    dirEntry = ttk.Entry(wnSortOpt, width=25, state="readonly")
+    dirSelectVar = StringVar()
+    dirSelect = ttk.Button(wnSortOpt, command=selectSpecificDir, textvariable=dirSelectVar)
+    dirSelectVar.set("Select folder")
 
-    # 3 comboboxes for selecting dates, with mindfulness of days in each month
     todaysDate = date.today()
     cal = Calendar(wnSortOpt, selectmode="day", year=todaysDate.year, month=todaysDate.month, day=todaysDate.day)
     
@@ -114,6 +116,8 @@ def addSortingOption():
 
         cal.grid_forget()
         cal.selection_set(todaysDate)
+
+        dirSelect.grid_forget()
 
         conditionResult.grid(column=2, row=2, padx=10, pady=40)
 
@@ -139,18 +143,45 @@ def addSortingOption():
         # Update if condition based on selection, unlock if condition and condition result
         conditionResult['state'] = "readonly"
         ifCondition['state'] = "readonly"
+
         if selectedSortOpt == selectionOptions[0]:
             ifCondition['values'] = ifConditions1
             ifConditionEntry.grid(column=2, row=2, padx=10, pady=40)
             conditionResult.grid(column=3, row=2, padx=10, pady=40)
+
         elif selectedSortOpt == selectionOptions[1]:
             ifCondition['values'] = ifConditions2
+
         elif selectedSortOpt == selectionOptions[2]:
             ifCondition['values'] = ifConditions3
             cal.grid(column=2, row=2, padx=10, pady=40)
             conditionResult.grid(column=3, row=2, padx=10, pady=40)
 
+    def conditionResultUpdate(self):
+        # Reset widgets
+        dirSelect.grid_forget()
+        dirSelectVar.set("Select folder")
+
+        conditionSelect = conditionResultVar.get()
+        conditionGridInfo = conditionResult.grid_info()
+
+        if conditionSelect == "Move file to specific folder...":
+            dirSelect.grid(column=conditionGridInfo["column"] + 1, row=2, padx=10)
+    
+    def fileTypeUpdate(self):
+        # Reset widgets
+        ifConditionEntry.grid_forget()
+
+        conditionResultVar.set("")
+        conditionResult.set("")
+
+        if ifConditionVar.get() == "If file is of specific type...":
+            ifConditionEntry.grid(column=2, row=2, padx=10, pady=40)
+            conditionResult.grid(column=3, row=2, padx=10, pady=40)
+
     selection.bind("<<ComboboxSelected>>", ifSelectionUpdate)
+    ifCondition.bind("<<ComboboxSelected>>", fileTypeUpdate)
+    conditionResult.bind("<<ComboboxSelected>>", conditionResultUpdate)
     wnSortOpt.resizable(False, False)
     
 # Frames
