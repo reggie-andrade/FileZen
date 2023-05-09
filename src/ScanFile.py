909c44de8ffe27e34a3aef149baad8a1d85e7ce0
sync144 <82534288+sync144@users.noreply.github.com>
2023-05-09 10:01:35 -0400

    Update ScanFile.py

--- src/ScanFile.py
+++ src/ScanFile.py
@@ -3,19 +3,21 @@ from tkinter import filedialog
 import os
 import datetime
 
+# TODO: fix file size bro
 class File:
     def __init__(self, fileInfo, fileName):
         self.name = fileName
-        self.type = fileInfo.st_mode
+        self.size = fileInfo.st_mode
         self.dateModified = fileInfo.st_atime
     
     def getInfo(self):
-        return ("Name: " + self.name, "Size: "+ str(self.type/10000) + " Kb", "Date Modified: " + datetime.datetime.fromtimestamp(self.dateModified).strftime('%m/%d/%Y %H:%M:%S %p'))
+        return ("Name: " + self.name, "Size: "+ str(self.size/10000) + " Kb", "Date Modified: " + datetime.datetime.fromtimestamp(self.dateModified).strftime('%m/%d/%Y %H:%M:%S %p'))
 
-def ScanFiles(fileObjs, txt): # scans chosen directory and appends file object with info on file
+# NOTE: If debugging, add "txt" as a parameter and add commented code back
+def ScanFiles(fileObjs): # scans chosen directory and appends file object with info on file
     """ Scans chosen directory and appends"""
-    if txt["state"] == "disabled":
-        txt.configure(state="normal")
+    # if txt["state"] == "disabled":
+    #     txt.configure(state="normal")
     fileObjs.clear()
     fileDirName = (filedialog.askdirectory())
     
@@ -24,7 +26,9 @@ def ScanFiles(fileObjs, txt): # scans chosen directory and appends file object w
             for file in files:
                 fileInfo = os.stat(os.path.join(root,file))
                 fileObjs.append(File(fileInfo, file))
-    for files in fileObjs:
-        txt.insert(END, f"{files.getInfo()}\n")
+    # for files in fileObjs:
+    #     txt.insert(END, f"{files.getInfo()}\n")
     
-    txt.configure(state="disabled")
+    # txt.configure(state="disabled")
+    for files in fileObjs:
+        print(f"{files.getInfo()}")
