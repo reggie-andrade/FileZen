ce3e8fa2be74db0a9d6be2ccb383ee3ba4008b3e
sync144 <82534288+sync144@users.noreply.github.com>
2023-05-11 07:06:36 -0400

    Test shorter ScanFile.py
    Testing a shorter method for scanning files

--- src/ScanFile.py
+++ src/ScanFile.py
@@ -3,32 +3,39 @@ from tkinter import filedialog
 import os
 import datetime
 
-# TODO: fix file size bro
-class File:
-    def __init__(self, fileInfo, fileName):
-        self.name = fileName
-        self.size = fileInfo.st_mode
-        self.dateModified = fileInfo.st_atime
+# # TODO: fix file size bro
+# class File:
+#     def __init__(self, fileInfo, fileName):
+#         self.name = fileName
+#         self.size = fileInfo.st_mode
+#         self.dateModified = fileInfo.st_atime
     
-    def getInfo(self):
-        return ("Name: " + self.name, "Size: "+ str(self.size/10000) + " Kb", "Date Modified: " + datetime.datetime.fromtimestamp(self.dateModified).strftime('%m/%d/%Y %H:%M:%S %p'))
+#     def getInfo(self):
+#         return ("Name: " + self.name, "Size: "+ str(self.size/10000) + " Kb", "Date Modified: " + datetime.datetime.fromtimestamp(self.dateModified).strftime('%m/%d/%Y %H:%M:%S %p'))
 
-# NOTE: If debugging, add "txt" as a parameter and add commented code back
-def ScanFiles(fileObjs): # scans chosen directory and appends file object with info on file
-    """ Scans chosen directory and appends"""
-    # if txt["state"] == "disabled":
-    #     txt.configure(state="normal")
-    fileObjs.clear()
-    fileDirName = (filedialog.askdirectory())
+# # NOTE: If debugging, add "txt" as a parameter and add commented code back
+# def ScanFiles(fileObjs): # scans chosen directory and appends file object with info on file
+#     """ Scans chosen directory and appends"""
+#     # if txt["state"] == "disabled":
+#     #     txt.configure(state="normal")
+#     fileObjs.clear()
+#     fileDirName = (filedialog.askdirectory())
     
-    for root, subdirs, files in os.walk(fileDirName):
-        if files != []: # Checks if list is empty and doesn't add it to fileObjects list
-            for file in files:
-                fileInfo = os.stat(os.path.join(root,file))
-                fileObjs.append(File(fileInfo, file))
-    # for files in fileObjs:
-    #     txt.insert(END, f"{files.getInfo()}\n")
+#     for root, subdirs, files in os.walk(fileDirName):
+#         if files != []: # Checks if list is empty and doesn't add it to fileObjects list
+#             for file in files:
+#                 fileInfo = os.stat(os.path.join(root,file))
+#                 fileObjs.append(File(fileInfo, file))
+#     # for files in fileObjs:
+#     #     txt.insert(END, f"{files.getInfo()}\n")
     
-    # txt.configure(state="disabled")
-    for files in fileObjs:
-        print(f"{files.getInfo()}")
+#     # txt.configure(state="disabled")
+#     for files in fileObjs:
+#         print(f"{files.getInfo()}")
+
+def ScanFiles(fileObjs):
+    fileDir = (filedialog.askdirectory())
+    fileObjs = os.listdir(fileDir)
+
+    for file in fileObjs:
+         print(file)
