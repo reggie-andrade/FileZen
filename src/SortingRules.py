75e58429f3190d9c6f2563a215d86cc7fd0641ae
sync144 <82534288+sync144@users.noreply.github.com>
2023-05-11 23:54:02 -0400

    Finalize SortingRules.py
    Refactored code for CustomTkinter, and finished DoesFileMatch() logic

--- src/SortingRules.py
+++ src/SortingRules.py
@@ -1,26 +1,25 @@
-from tkinter import *
-from tkinter import filedialog
-from tkinter import ttk
-from tkcalendar import Calendar
-from datetime import date
+import tkinter
+import customtkinter as cstk
+import datetime as dt
+import time
+import os
 import Sort
 
 # TODO: Add 'ignore files based on...'
 class SortingRule:
-    def __init__(self, sortingOption, ifcondition, ext, date, result, directory):
+    def __init__(self, sortingOption, ifcondition, entry, result, directory):
         # Setup step variables
         self.count = 0
         # Checking step variables
         self.sortingOption = sortingOption
         self.ifcondition = ifcondition
-        self.entry = ext
-        self.date = date
+        self.entry = entry
         # Final step variables
         self.result = result
         self.directory = directory
     
     # Check if the passed file matches criteria of rule
-    def DoesFileMatch(self, fileName):
+    def DoesFileMatch(self, fileName, fileDir):
         match = False
 
         baseOptions = (
@@ -66,26 +65,54 @@ class SortingRule:
                 if Sort.isExtension(fileName, self.entry):
                     match = True
         elif self.sortingOption == baseOptions[2]:
-            print("Sorting based on date")
+            ifConditions = (
+                "If file was created before...",
+                "If file was created after...",
+                "If file was created on...",
+            )
+
+            givenDate = dt.datetime.strptime(self.entry, "%m/%d/%Y")
+            creationDate = dt.datetime.fromtimestamp(os.path.getctime(fileDir + "/" + fileName))
+            creationDate = creationDate.replace(hour=0, minute=0, second=0, microsecond=0)
+
+            if self.ifcondition == ifConditions[0]:
+                if creationDate < givenDate:
+                    match = True
+            elif self.ifcondition == ifConditions[1]:
+                if creationDate > givenDate:
+                    match = True
+            elif self.ifcondition == ifConditions[2]:
+                if creationDate == givenDate:
+                    match = True
         return match
 
+    def GetResult(self):
+        return self.result
+    
+    def GetTargetDir(self):
+        return self.directory
+    
     def __str__(self):
         return (
-            f"SORTINGRULE {self.count}\n sortingOption: {self.sortingOption}\n ifcondition: {self.ifcondition}\n entry: {self.entry}\n date: {self.date}\n result: {self.result}\n directory: {self.directory}"
+            f"SORTINGRULE {self.count}\n sortingOption: {self.sortingOption}\n ifcondition: {self.ifcondition}\n entry: {self.entry}\n result: {self.result}\n directory: {self.directory}"
         )
         
-        
+
 
 def addSortingOption(toplevel):
     # Setup ------------------------------------------------------------------------------------------------------------
     rule = None
 
     # New window setup
-    wnSortOpt = Toplevel(toplevel)
+    cstk.set_appearance_mode("dark") 
+    cstk.set_default_color_theme("C:/Users/Karat/OneDrive/Desktop/FileSort/FileSort-main/src/theme.json")
+
+    wnSortOpt = cstk.CTkToplevel()
     wnSortOpt.title("Add file sorting rule")
-    wnSortOptWidth = 1100
-    wnSortOptHeight = 450
+    wnSortOptWidth = 800
+    wnSortOptHeight = 350
     wnSortOpt.geometry(f"{wnSortOptWidth}x{wnSortOptHeight}")
+    wnSortOpt.attributes("-topmost", True)
 
     # Functions ------------------------------------------------------------------------------------------------------------
 
@@ -110,41 +137,35 @@ def addSortingOption(toplevel):
         )
 
         # Reset widgets
-        ifConditionVar.set("")
         ifCondition.set("")
 
-        conditionResultVar.set("")
         conditionResult.set("")
 
-        ifEntryVar.set("")
-        ifConditionEntry.delete(0, END)
-        ifConditionEntry.grid_forget()
-
-        cal.grid_forget()
-        cal.selection_set(todaysDate)
+        caseEntryVar.set("")
+        caseEntry.delete(0, cstk.END)
+        caseEntry.grid_forget()
 
         dirSelect.grid_forget()
 
         conditionResult.grid(column=2, row=2, padx=10, pady=40)
 
         # Get if statement selection, change if statement
-        selectedSortOpt = selectVar.get()
+        selectedSortOpt = selection.get()
 
         # Update if condition based on selection, unlock if condition and condition result
-        conditionResult['state'] = "readonly"
-        ifCondition['state'] = "readonly"
+        conditionResult.configure(state = "readonly")
+        ifCondition.configure(state = "readonly")
 
         if selectedSortOpt == selectionOptions[0]:
-            ifCondition['values'] = ifConditions1
-            ifConditionEntry.grid(column=2, row=2, padx=10, pady=40)
+            ifCondition.configure(values = ifConditions1)
+            caseEntry.grid(column=2, row=2, padx=10, pady=40)
             conditionResult.grid(column=3, row=2, padx=10, pady=40)
-
         elif selectedSortOpt == selectionOptions[1]:
-            ifCondition['values'] = ifConditions2
-
+            ifCondition.configure(values = ifConditions2)
         elif selectedSortOpt == selectionOptions[2]:
-            ifCondition['values'] = ifConditions3
-            cal.grid(column=2, row=2, padx=10, pady=40)
+            ifCondition.configure(values = ifConditions3)
+            caseEntry.grid(column=2, row=2, padx=10, pady=40)
+            caseEntry.insert(0, "O/OO/OOOO")
             conditionResult.grid(column=3, row=2, padx=10, pady=40)
 
     # Function to cause the directory selection button to appear if the condition result is "Move file to specific folder"
@@ -153,7 +174,7 @@ def addSortingOption(toplevel):
         dirSelect.grid_forget()
         dirSelectVar.set("Select folder")
 
-        conditionSelect = conditionResultVar.get()
+        conditionSelect = conditionResult.get()
         conditionGridInfo = conditionResult.grid_info()
 
         if conditionSelect == "Move file to specific folder...":
@@ -161,54 +182,49 @@ def addSortingOption(toplevel):
     
     # Function to cause the if condition entry box to appear if any conditions are true
     def ifEntryUpdate(self):
-        ifConditions1 = (
-            "If file name starts with...",
-            "If file name ends with...",
-            "If file name contains...",
-        )
-
         # Reset widgets
-        ifConditionEntry.grid_forget()
+        caseEntry.grid_forget()
 
-        conditionResultVar.set("")
         conditionResult.set("")
         
-        if ifConditionVar.get() == "If file is of specific type...":
-            ifConditionEntry.grid(column=2, row=2, padx=10, pady=40)
+        if ifCondition.get() == "If file is of specific type...":
+            caseEntry.grid(column=2, row=2, padx=10, pady=40)
             conditionResult.grid(column=3, row=2, padx=10, pady=40)
-        else:
-            for opt in ifConditions1:
-                if ifConditionVar.get() == opt:
-                    ifConditionEntry.grid(column=2, row=2, padx=10, pady=40)
-                    conditionResult.grid(column=3, row=2, padx=10, pady=40)
+        elif selection.get() == "Sort files based on date":
+            caseEntry.grid(column=2, row=2, padx=10, pady=40)
+            caseEntry.delete(0, cstk.END)
+            caseEntry.insert(0, "O/OO/OOOO")
+            conditionResult.grid(column=3, row=2, padx=10, pady=40) 
+        elif selection.get() == "Sort files based on name":
+            caseEntry.grid(column=2, row=2, padx=10, pady=40)
+            conditionResult.grid(column=3, row=2, padx=10, pady=40)
     
     # Function to select a specific directory, used if condition result "Move file to specific folder" is used
     def selectSpecificDir():
-        selectedFolder = filedialog.askdirectory()
+        wnSortOpt.attributes("-topmost", False)
+        selectedFolder = cstk.filedialog.askdirectory()
         dirSelectVar.set(selectedFolder)
-        wnSortOpt.lift()
+        wnSortOpt.attributes("-topmost", True)
 
     # Widgets ------------------------------------------------------------------------------------------------------------
 
     # Primary selection dropdown for file sorting
-    selectVar = StringVar()
-    selection = ttk.Combobox(wnSortOpt, width=27, state="readonly", textvariable=selectVar)
+    
+    selection = cstk.CTkComboBox(wnSortOpt, width=180, state="readonly", command=ifSelectionUpdate)
     selectionOptions = (
         "Sort files based on name",
         "Sort files based on type",
         "Sort files based on date",
     )
-    selection['values'] = selectionOptions
+    selection.configure(values = selectionOptions)
     selection.grid(column=1, row=1, padx=10, pady=10)
 
     # If conditions dropdown based on primary selection
-    ifConditionVar = StringVar()
-    ifCondition = ttk.Combobox(wnSortOpt, width=27, state="disabled", textvariable=ifConditionVar)
+    ifCondition = cstk.CTkComboBox(wnSortOpt, width=180, state="disabled", command=ifEntryUpdate)
     ifCondition.grid(column=1, row=2, padx=10, pady=40)
 
     # Condition result dropdown based on if condition
-    conditionResultVar = StringVar()
-    conditionResult = ttk.Combobox(wnSortOpt, width=27, state="disabled", textvariable=conditionResultVar)
+    conditionResult = cstk.CTkComboBox(wnSortOpt, width=180, state="disabled")
     conditionResultOptions = (
         "Move file to desktop",
         "Move file to videos",
@@ -216,32 +232,23 @@ def addSortingOption(toplevel):
         "Move file to specific folder...",
         "Move file to recycling bin"
     )
-    conditionResult['values'] = conditionResultOptions
+    conditionResult.configure(values = conditionResultOptions, command=dirSelectUpdate)
     conditionResult.grid(column=2, row=2, padx=10, pady=40)
 
-    # If condition entry box, only visible if using a specific if condition
-    ifEntryVar = StringVar()
-    ifConditionEntry = ttk.Entry(wnSortOpt, width=17, textvariable=ifEntryVar)
+    # Entry box, only visible if using a specific if condition
+    caseEntryVar = cstk.StringVar()
+    caseEntry = cstk.CTkEntry(wnSortOpt, width=150, textvariable=caseEntryVar)
 
     # Directory select button, only shows if "Move file to specific folder" is used
-    dirSelectVar = StringVar()
-    dirSelect = ttk.Button(wnSortOpt, command=selectSpecificDir, textvariable=dirSelectVar)
+    dirSelectVar = cstk.StringVar()
+    dirSelect = cstk.CTkButton(wnSortOpt, command=selectSpecificDir, textvariable=dirSelectVar)
     dirSelectVar.set("Select folder")
 
-    # Visual calandar used for if primary selection is "Sort files based on date"
-    todaysDate = date.today()
-    cal = Calendar(wnSortOpt, selectmode="day", year=todaysDate.year, month=todaysDate.month, day=todaysDate.day)
-
     # Submit button
-    submitVar = IntVar()
-    submit = ttk.Button(wnSortOpt, command=lambda: submitVar.set(1))
+    submitVar = cstk.IntVar()
+    submit = cstk.CTkButton(wnSortOpt, command=lambda: submitVar.set(1))
     submit.configure(text="Submit")
     submit.grid(column=1, row=3, padx=10, pady=10)
-
-    # Bindings
-    selection.bind("<<ComboboxSelected>>", ifSelectionUpdate)
-    ifCondition.bind("<<ComboboxSelected>>", ifEntryUpdate)
-    conditionResult.bind("<<ComboboxSelected>>", dirSelectUpdate)
 
     # Finalization
     wnSortOpt.resizable(False, False)
@@ -249,11 +256,10 @@ def addSortingOption(toplevel):
     submit.wait_variable(submitVar)
 
     rule = SortingRule(
-        selectVar.get(),
-        ifConditionVar.get(),
-        ifEntryVar.get(),
-        cal.get_date(),
-        conditionResultVar.get(),
+        selection.get(),
+        ifCondition.get(),
+        caseEntryVar.get(),
+        conditionResult.get(),
         dirSelectVar.get()
     )
 
